f6671336852d943d12f605c9d02961a95e7de24d
DeskYT <deskytube@gmail.com>
2021-04-28 08:03:10 +0300

    Add createElement attributes

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @param {string} tagName
+ * @param {object} options
+ * @param {string[]} options.classNames - css classes
+ * @param {object} options.handlers - event handlers
+ * @param {object} options.attributes - attributes object
+ * @param  {...Node} children
+ * @returns {HTMLElement}
+ */
+ function createElement(
+  tagName,
+  { classNames = [], handlers = {}, attributes = {} } = {},
+  ...children
+) {
+  const elem = document.createElement(tagName);
+  elem.classList.add(...classNames);
+
+  for (const [attrName, attrValue] of Object.entries(attributes)) {
+    elem.setAttribute(attrName, attrValue);
+  }
+
+  for (const [eventType, eventHandler] of Object.entries(handlers)) {
+    elem.addEventListener(eventType, eventHandler);
+  }
+
+  elem.append(...children);
+  return elem;
+}
